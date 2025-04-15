afbbb0674893bae7aca5bf57489bb0d5cb1be3a2
Garrett Sward <garrett.sward@gmail.com>
2025-04-15 15:35:58 -0400

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+
+<!-- Compressed Foundations CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.9.0/dist/css/foundation.min.css" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/03-28-2025.css">
+
+</head>
+<body>
+
+<div class="grid-y" style="height: 40px;">
+</div>
+
+<div class="grid-x grid-padding-x">
+	<div class="cell small-12 large-4 large-offset-4">
+		<h2><a href="./src/03-10-2025_workday_workbench.php">Helpful Quicklinks</a></h2>
+		<ul>
+			<li><a target="_blank" href="https://www.nytimes.com/crosswords">NYT Games</a></li>
+		</ul>
+	</div>
+</div>
+
+</body>
+</html>
