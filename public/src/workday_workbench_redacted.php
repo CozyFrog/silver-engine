f9b07b7de9130a8cb0ce5988f3e1cdc0103eee1a
Garrett Sward <garrett.sward@gmail.com>
2025-05-12 12:06:52 -0400

    Create workday_workbench_redacted.php

---
+++ public/src/workday_workbench_redacted.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<head>
+
+<!-- Compressed Foundations CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.9.0/dist/css/foundation.min.css" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/03-28-2025.css">
+
+</head>
+<body>
+<div class="grid-y" style="height: 40px;">
+
+</div>
+<div class="grid-x grid-padding-x">
+	<div class="cell small-12 large-4 large-offset-2">
+		<h2><a target="_blank" href="https://impl.workday.com/wfu10/d/home.htmld">WFU10</a></h2>
+		<ul>
+			<li><a target="_blank" href='https://impl.workday.com/wfu10/d/task/XXXX$YYYYY.htmld'>Run GS Rep. Cat.</a></li>
+			<li><a target="_blank" href='https://impl.workday.com/wfu10/d/task/ZZZZ$YYYY.htmld'>Maintain RCFs</a></li>
+			<li><a target="_blank" href='https://impl.workday.com/wfu10/d/task/ZZZZ$YYYY.htmld'>Bus. Object Details</a></li>
+			<li><a target="_blank" href='https://impl.workday.com/wfu10/d/inst/WWWW$YYYYY/rel-task/WWWW$YYY.htmld'>Edit <strong>GS - Course Schedule Foundation</strong></a></li>
+		</ul>
+		<h2><a target="_blank" href="">Sandbox</a></h2>
+		<ul>
+			<li><a target="_blank" href='https://impl.workday.com/wfu/d/inst/KKKK$YYYYY/rel-task/JJJJ$YYY.htmld'>Edit Grades Report</a></li>
+		</ul>
+		<h2><a target="_blank" href="https://help.wfu.edu/support/home">WFU Support</a></h2>
+		<ul>
+			<li><a target="_blank" href='https://help.wfu.edu/support/catalog/items/159'>Request Report Migration</a></li>
+		</ul>
+	</div>
+
+	<div class="cell small-12 large-4">
+		<h2><a target="_blank" href="https://drive.google.com/drive/u/0/folders/1vKyOtwijT36vJpwchHIqK68kPFe-AKsS">Law WD Reporting Docs</h2>
+		<ul>
+			<li><a target="_blank" href="https://docs.google.com/document/d/1ryrFw6A9O_L-VJb-05kgBPyZVFl9cP4OfyIA4l3gTss/edit?tab=t.0">Course Schedule Foundation Report Spec Notes</a></li>
+				<ul><li><p>From meeting 03/11/2025 w/ Margaret Taylor, report requestor.</p></li></ul>
+		</ul>
+	</div>
+</div>
+
+</body>
